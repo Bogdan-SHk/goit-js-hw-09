08b1119f8861f962549e9b3893ce77bfb440c1a7
Bogdan-SHk <bogdan.shkolnik@gmail.com>
2023-02-20 22:25:52 +0200

    Виконав завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const buttonStartEl = document.querySelector("[data-start]");
+const buttonStopEl = document.querySelector("[data-stop]");
+let timerId = null;
 
+
+buttonStartEl.addEventListener('click', onButtonStart);
+
+function onButtonStart() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+    buttonStartEl.disabled = true;
+}
+
+
+buttonStopEl.addEventListener('click', onButtonStop);
+
+function onButtonStop() {
+    clearInterval(timerId);
+    buttonStartEl.disabled = false;
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
